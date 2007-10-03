cdee6135761a656d6644b735934fd523325f0652
Stephan Richter <stephan.richter@gmail.com>
2007-10-03 14:29:39 +0000

    - Get meta-data ready for release.
    - There is a new test failure due to some dependency issues that I 
      have to figure out later.

--- README.txt
+++ README.txt
@@ -1,7 +1,1 @@
-zope.viewlet Package Readme
-===========================
-
-Overview
---------
-
 Viewlets provide a generic framework for building pluggable user interfaces.
--- setup.py
+++ setup.py
@@ -15,35 +15,49 @@
 
 $Id$
 """
-
 import os
-
 from setuptools import setup, find_packages
 
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
 setup(name='zope.viewlet',
-      version = '3.4.0b1',
-      url='http://svn.zope.org/zope.viewlet',
-      license='ZPL 2.1',
-      description='Zope viewlet',
+      version = '3.4.0',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
-      long_description="Viewlets provide a generic framework for"
-                       "building pluggable user interfaces.",
-
+      description='Zope Viewlets',
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope3 web html ui viewlet pattern",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://cheeseshop.python.org/pypi/zope.viewlet',
+      license='ZPL 2.1',
       packages=find_packages('src'),
-	  package_dir = {'': 'src'},
-
-      namespace_packages=['zope',],
-      extras_require = dict(test=['zope.app.testing',
-                                  'zope.testing',
-                                  'zope.app.securitypolicy',
-                                  'zope.app.zcmlfiles']),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope'],
+      extras_require = dict(
+          test=['zope.app.testing',
+                'zope.testing',
+                'zope.app.securitypolicy',
+                'zope.app.zcmlfiles']),
       install_requires=['setuptools',
                         'zope.app.pagetemplate',
                         'zope.app.publisher',
                         'zope.component',
                         'zope.configuration',
-                        'zope.contentprovider>=3.4.0b1.dev-r75494',
+                        'zope.contentprovider',
                         'zope.event',
                         'zope.i18nmessageid',
                         'zope.interface',
