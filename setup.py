39283cd4b7d417e37b571abf7edd5505d0cffc47
Stephan Richter <stephan.richter@gmail.com>
2007-10-11 04:06:12 +0000

    Silly version change.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.viewlet',
-      version = '3.4.0',
+      version = '3.5.0dev',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
       description='Zope Viewlets',
