9f292880501b4da655c02cb8a797a448d6fe8aca
Tres Seaver <tseaver@palladion.com>
2016-03-24 11:23:26 -0400

    Drop support for Python 2.6.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def read(*rnames):
         return f.read()
 
 setup(name='zope.viewlet',
-      version='4.0.1.dev0',
+      version='4.1.0.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Viewlets',
@@ -47,7 +47,6 @@ setup(name='zope.viewlet',
           'License :: OSI Approved :: Zope Public License',
           'Programming Language :: Python',
           'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.6',
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.3',
