d13540ab98f357bc94f057e916efd4b4da0119b4
Michael Howitz <mh@gocept.com>
2023-03-27 13:46:25 +0200

    Back to development: 5.1

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ TESTS_REQUIRE = [
 ]
 
 setup(name='zope.viewlet',
-      version='5.0',
+      version='5.1.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.dev',
       description='Zope Viewlets',
