0171101dc2656e675896c27c37bd1761ce884321
Michael Howitz <icemac@gmx.net>
2025-02-14 10:43:19 +0100

    Preparing release 5.1

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ TESTS_REQUIRE = [
 ]
 
 setup(name='zope.viewlet',
-      version='5.1.dev0',
+      version='5.1',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.dev',
       description='Zope Viewlets',
