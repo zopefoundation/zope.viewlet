8fe00a039b10fe125bffd3ecbc45df9beb9abc4f
Michael Howitz <icemac@gmx.net>
2025-09-12 09:51:47 +0200

    Back to development: 6.1

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ TESTS_REQUIRE = [
 ]
 
 setup(name='zope.viewlet',
-      version='6.0',
+      version='6.1.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.dev',
       description='Zope Viewlets',
